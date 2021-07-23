906ccf0fb6f64016c743062e82e59f0d9bf668c6
Igor-Cruz <igorcruz.it@outlook.com>
2021-07-22 22:49:16 -0300

    Projeto finalizado]

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-document.querySelector('.busca').addEventListener('submit', event => {
+document.querySelector('.busca').addEventListener('submit', async event => {
   event.preventDefault() //Previne o comportamento padrão
 
   let input = document.querySelector('#searchInput').value
 
   if (input !== '') {
+    clearInfo()
     showWarning('Carregando...')
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
       input
-    )}&appid=46e4cc1ba1d2c78229fa497c0fa5e589&units=metric&lang=pt_br`
+    )}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`
+
+    let results = await fetch(url)
+    let json = await results.json()
+
+    if (json.cod === 200) {
+      showInfo({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        temIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windAngle: json.wind.deg
+      })
+    } else {
+      clearInfo()
+      showWarning('Não encontramos esta localização')
+    }
+  } else {
+    clearInfo()
   }
 })
 
+function showInfo(json) {
+  showWarning('')
+  document.querySelector('.resultado').style.display = 'block'
+
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+
+  document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+
+  document.querySelector(
+    '.ventoInfo'
+  ).innerHTML = `${json.windSpeed} <span>km/h</span>`
+
+  document
+    .querySelector('.temp img')
+    .setAttribute(
+      'src',
+      `http://openweathermap.org/img/wn/${json.temIcon}@2x.png`
+    )
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`
+}
+
 function showWarning(msg) {
   document.querySelector('.aviso').innerHTML = msg
+}
+
+function clearInfo() {
+  showWarning('')
+
+  document.querySelector('.resultado').style.display = 'none'
 }
